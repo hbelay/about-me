85a2f73e8f0278cba880ebad9a65f6e3d44539ee
Haimanot Belay <belayhm12@gmail.com>
2016-05-10 14:31:04 -0700

    second commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var userName = prompt('Hi thanks for vidsiting me About Me page? What is your name? ');
+alert('nice to meet you' + userName + '. I\'m going to ask you sime questions about me. Really/ Make your best guess. ');
+console.log('The user \'s name is ' + userName + ' and ');
+
+//var userName = 'test';
+
+var response1 = prompt('do i have 3 cat ');
+var answer1 = 'yes';
+if(response1 === answer1){
+  alert('you are correct, ' + userName + ' ! my cat are buddy, tty and ');
+}else{
+  alert('nope!' + userName + ' .I Really');
+}
