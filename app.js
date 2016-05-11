6052a6044de6fb1a28bf83b10a793e3eae3baa2a
Haimanot Belay <belayhm12@gmail.com>
2016-05-10 21:31:08 -0700

    Practice 3rd

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-var userName = prompt('Hi thanks for vidsiting me About Me page? What is your name? ');
-alert('nice to meet you' + userName + '. I\'m going to ask you sime questions about me. Really/ Make your best guess. ');
-console.log('The user \'s name is ' + userName + ' and ');
+var userName = prompt('Hi there, thanks for vidsiting About Me page? What is your name? ');
+alert('nice to meet you ' + userName + '. I\'m going to ask you some questions about me. Really. Make your best guesses. Please answer yes/no ');
+console.log('The user \'s name is ' + userName + ' and he is gruessing my questions');
 
-//var userName = 'test';
-
-var response1 = prompt('do i have 3 cat ');
+var response1 = prompt('Do I have kids? ');
 var answer1 = 'yes';
-if(response1 === answer1){
-  alert('you are correct, ' + userName + ' ! my cat are buddy, tty and ');
-}else{
-  alert('nope!' + userName + ' .I Really');
+if(response1.toLowerCase() === answer1){
+  alert('Your response is ' + response1 + ' you are correct, ' + userName + ' ! I have 2 kids');
+
+}else {
+  alert('Nope!' + userName + ' . you don\'t get this right');
+}
+var response2 = prompt('Have I been arrested? ');
+var answer2 = 'no';
+if(response2.toLowerCase() === answer2){
+  alert('Your response is ' + response2 + ' you are correct, ' + userName + ' ! I have not been arested');
+
+}else {
+  alert('Nope!' + userName + ' . you don\'t get this right');
+}
+var response3 = prompt('Am I a good singer? ');
+var answer3 = 'no';
+if(response3.toLowerCase() === answer3){
+  alert('Your response is ' + response3 + ' you are correct, ' + userName + ' ! I\'m not');
+
+}else {
+  alert('Nope!' + userName + ' . you don\'t get this right');
+}
+var response4 = prompt('Do I have siblings? ');
+var answer4 = 'yes';
+if(response4.toLowerCase() === answer4){
+  alert('Your response is ' + response4 + ' you are correct, ' + userName + '  I have 2 5 Siblings');
+}else {alert('Nope!' + userName + ' . you don\'t get this right');
+}
+var response5 = prompt('Do I like talking to friends on facebook? ');
+var answer5 = 'no';
+if(response5.toLowerCase() === answer5){
+  alert('Your response is ' + response5 + ' you are correct, ' + userName + ' ! I don\'t');
+
+}else {
+  alert('Nope!' + userName + ' . you don\'t get this right');
 }
