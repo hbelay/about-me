803e33cdfa04a59cf613d6c2529325597671c005
Haimanot Belay <belayhm12@gmail.com>
2016-05-15 12:02:46 -0700

    Resume cleanup

--- app.js
+++ app.js
@@ -1,40 +1,65 @@
+var score = 0;
 var userName = prompt('Hi there, thanks for visiting About Me page? What is your name? ').toUpperCase();
-alert('Nice to meet you ' + userName + '. I\'m going to ask you some questions about me. Make your best guesses.  Please give answer yes/no! ');
+alert('Nice to meet you ' + userName + '. I\'m going to ask you 7 questions about me. Make your best guesses.  Please give answer accordingly ');
 console.log('The user \'s name is ' + userName + ' and he is guessing my questions');
 
-var response1 = prompt('Do I live in Seattle?').toLowerCase();
-if(response1 === 'yes' || response1 === 'y'){
-  alert('Your response is ' + response1 + '! You are correct, ' + userName + ' ! I live in Seattle, WA.');
-}else {
-  alert('Nope! ' + userName + ' . You don\'t get this right');
+var questions = ['Do I love Soccer?', 'Have I been in New York?', 'Am I a good singer?',
+                'Do I have siblings?', 'Do I like talking to friends on facebook?'];
+var answers   = ['yes', 'no', 'no', 'yes', 'yes'];
+var responses = ['I\'m crazy Arsenal Fc fun, ', 'I have not been in New York', 'I\'m not a good singer',
+                'I have 5 Siblings', 'No, I don\'t like Facebook'];
+
+function yesNoQuestions() {
+  for (i = 0; i < 5; i++) {
+    var ques = prompt(questions[i]).toLowerCase();
+    console.log(ques, answers[i], answers[i][0]);
+    if (ques === answers[i] || ques === answers[i][0]){
+      alert(responses[i]);
+      score++;
+    } else {
+      alert('Nope, that is not correct. The correct answer is ' + responses[i]);
+    }
+  }
 }
-var response2 = prompt('Have I been in New York? ').toLowerCase();
-if(response2 === 'no' || response2 === 'n'){
-  alert('Your response is ' + response2 + ', and you are correct, ' + userName + ' ! I have not been in New York');
+yesNoQuestions();
 
-}else {
-  alert('Nope! ' + userName + ',  you don\'t get this right');
+// #6
+var number = 25;
+for(var i = 0; i < 4; i++){
+  console.log(number);
+  var response6 = parseInt(prompt('Guess my number between 1 to 100'));
+  if (number === response6) {
+    alert ('Great! you get it. ');
+    score++;
+    break;
+  } else if(number < response6){
+    alert('Hi,' + userName + ' it is high and you have ' + (4 - i) + ' Please try again? ');
+  }else if(number > response6){
+    alert('Hi,' + userName + ' it is low and you have ' + (4 - i) + ' Please try again? ');
+  }else {
+    alert('This is not a number, and you have ' + (4 - i) + ' Please try again? ');
+  }
 }
-
-var response3 = prompt('Am I a good singer? ').toLowerCase() ;
-if(response3 === 'no' || response3 === 'n'){
-  alert('Your response is ' + response3 + ' you are correct ' + userName + '! I\'m not');
-
-}else {
-  alert('Nope!' + userName + ', you don\'t get this right');
+//# 7
+var array = ['LOS ANGLES', 'LONDON', 'NEW YORK', 'CHICOGO', 'BOSTON', 'SEATTLE','PARIS', 'DUBAI'];
+for(var i = 0; i < 6; i++){
+  var response7 = prompt('Guess the cities I have visited? ').toUpperCase();
+  for(var j = 0; j < array.length; j++){
+    console.log(array);
+    if (array[j] === response7) {
+      alert('Great, you get it!');
+      score++;
+      var correctAnswer = true;
+      break;
+    }
+  }
+  if (correctAnswer) {
+    break;
+  }
+  alert('Try again! You have ' + (5 - i) + ' trials.');
 }
-
-var response4 = prompt('Do I have siblings? ').toLowerCase();
-if(response4 === 'yes' || response4 === 'y'){
-  alert('Your response is ' + response4 + ' you are correct ' + userName + ',  I have 2 5 Siblings');
+if(score > 5){
+  alert('You answered ' + score + ' out of 7 questions correctly ' + userName + ', Well Done! Thak you and see you next time.');
 }else {
-  alert('Nope!' + userName + ', you don\'t get this right');
-}
-
-var response5 = prompt('Do I like talking to friends on facebook? ').toLowerCase() ;
-if(response5 === 'yes' || response5 === 'n'){
-  alert('Your response is ' + response5 + ', you are correct ' + userName + '! I don\'t like Facebook');
-
-}else {
-  alert('Nope!' + userName + ', you don\'t get this right');
+  alert('You answered ' + score + ' out of 7 questions correctly ' + userName + ', Thak you and see you next time.');
 }
